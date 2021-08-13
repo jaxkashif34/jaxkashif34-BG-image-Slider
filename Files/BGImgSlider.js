9688e25e5237b48d6871b03ec9eed8e429bc353b
Kashif Ali <jaxkashif34@gmail.com>
2021-08-13 14:00:36 -0700

    Final Commit

---
+++ Files/BGImgSlider.js
@@ -0,0 +1,27 @@
+const pictures = [
+    "contBcg-0",
+    "contBcg-1",
+    "contBcg-2",
+    "contBcg-3",
+    "contBcg-4",
+    "contBcg-5"
+]
+$(document).ready(function () {
+    let counter = 0;
+    $(".BtnLeft").click(function (e) {
+        e.preventDefault();
+        counter--;
+        if (counter < 0) {
+            counter = pictures.length - 1;
+        }
+        $(".imgContainer").css("backgroundImage", `url('./images/${pictures[counter]}.jpg')`);
+    });
+    $(".BtnRight").click(function (e) {
+        e.preventDefault();
+        counter++
+        if (counter > pictures.length - 1) {
+            counter = 0
+        }
+        $(".imgContainer").css("backgroundImage", `url('./images/${pictures[counter]}.jpg')`);
+    });
+});
